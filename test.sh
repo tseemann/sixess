7cf1ade4cbdabc046eadab57539f96cb63efcdcd
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-24 21:03:01 +1000

    Document test script

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# this reads the script: section of the .travis.yml file
+# and runs all the tests locally instead
+# can run from root directory with: ./test.sh |& grep '###'
+
 DIR=$(dirname $0)
 YAML="$DIR/.travis.yml"
 
@@ -9,5 +13,4 @@ do
   bash -c "$CMD"
   echo "### TEST RESULT: $?"
   echo "### END TEST"
-#  $$CMD
 done < <(grep sixess "$YAML" | sed 's/^[^"]*"//' | sed 's/"$//')
